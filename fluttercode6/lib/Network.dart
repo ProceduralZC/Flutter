60636c9b82a77ce68a4715b5c333030f8f104a20
2079904207@qq.com <2079904207@qq.com>
2019-10-09 22:28:24 +0800

    1.Flutter 网络通讯和网络封装

--- fluttercode6/lib/Network.dart
+++ fluttercode6/lib/Network.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:convert' as convert;
+import 'package:fluttercode6/utils/HttpUtils.dart';
 
 class Network extends StatefulWidget {
   final String title;
@@ -11,6 +13,60 @@ class Network extends StatefulWidget {
 class MyNetworkState extends State<Network> {
   String title;
   MyNetworkState({Key key,this.title});
+
+  /*获取数据接口的调用*/
+  // GET 请求
+// 返回的结果直接就是 json 格式
+// 要使用 await，必须在方法名后面加上 async
+  _dioGetRequest () async {
+    var result = await HttpUtils.request(
+        'username=admin&password=123',
+        method: HttpUtils.GET
+    );
+    setState(() {
+      var content = result;
+      print("userinfo="+content.toString());
+      String josnNameString = convert.jsonEncode(content.toString());/*装换成json数据，做解析就行了*/
+      print("token="+josnNameString);
+
+    });
+  }
+
+
+// POST 请求
+  _dioAddRequest () async {
+    var result = await HttpUtils.request(
+        'username=admin&password=123',
+        method: HttpUtils.POST,
+        data: {
+          'username': "admin",
+          'password': "123",
+        }
+    );
+  }
+
+// PUT 请求
+  _dioEditRequest () async{
+    var result = await HttpUtils.request(
+        'username=admin&password=123',
+        method: HttpUtils.PUT
+    );
+  }
+
+// DELETE 请求
+  _dioDelRequest () async {
+    var result = await HttpUtils.request(
+        'username=admin&password=123',
+        method: HttpUtils.DELETE
+    );
+  }
+
+  //初始化状态
+  @override
+  void initState() {
+    super.initState();
+    _dioGetRequest ();
+  }
 
   @override
   Widget build(BuildContext context) {
